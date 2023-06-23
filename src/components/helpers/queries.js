7d35329925e9e563898dbe1b69a842288db175e8
Silvio Ruiz <silviojrck@gmail.com>
2023-06-23 00:25:05 -0300

    Agrega logica login

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,16 +1,17 @@
 const URL_receta = import.meta.env.VITE_RECETA;
+const URL_usuario = import.meta.env.VITE_USUARIO;
 
-export const obtenerRecetas = async ()=>{
+export const obtenerRecetas = async () => {
     try {
         const respuesta = await fetch(URL_receta);
         const listaRecetas = await respuesta.json();
         return listaRecetas;
-    }catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
 
-export const crearReceta = async (receta)=>{
+export const crearReceta = async (receta) => {
     try {
         const respuesta = await fetch(URL_receta, {
             method: "POST",
@@ -20,7 +21,28 @@ export const crearReceta = async (receta)=>{
             body: JSON.stringify(receta)
         });
         return respuesta;
-    }catch(error){
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const login = async (usuario) => {
+    try {
+        const respuesta = await fetch(URL_usuario);
+        const listaUsuarios = await respuesta.json();
+        const usuarioBuscado = listaUsuarios.find((itemUsuario) => itemUsuario.email === usuario.email);
+        if (usuarioBuscado) {
+            if (usuarioBuscado.password === usuario.password) {
+                return usuarioBuscado
+            } else {
+                console.log('el password es incorrecto')
+                return null
+            }
+        } else {
+            console.log('el mail no existe')
+            return null
+        }
+    } catch (error) {
         console.log(error)
     }
 }
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,20 +1,52 @@
 import { Container, Card, Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { login } from "../helpers/queries";
 
-const Login = () => {
+const Login = ({ setUsuarioLogueado }) => {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const navegacion = useNavigate();
+
+    const onSubmit = (usuario) => {
+        login(usuario).then((respuesta) => {
+            console.log(respuesta)
+            if (respuesta) {
+                sessionStorage.setItem('usuario', JSON.stringify(respuesta));
+                setUsuarioLogueado(respuesta);
+                navegacion('/administrador');
+            } else {
+                Swal.fire(
+                    'Ocurrio un error',
+                    'El email o usuario son erroneos',
+                    'error'
+                )
+            }
+        })
+    }
+
     return (
         <Container className="main">
             <Card className="my-5">
                 <Card.Header className="text-light bg-dark" as="h5">Login</Card.Header>
                 <Card.Body>
-                    <Form>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Ingrese un email"
+                                {...register('email', {
+                                    required: 'El email es un dato obligatorio',
+                                    pattern: {
+                                        value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                                        message: 'El email debe tener un formato valido (mail@dominio.com)'
+                                    }
+                                })}
                             />
                             <Form.Text className="text-danger">
-                               error
+                                {errors.email?.message}
                             </Form.Text>
                         </Form.Group>
 
@@ -23,9 +55,18 @@ const Login = () => {
                             <Form.Control
                                 type="password"
                                 placeholder="Password"
+                                {
+                                ...register('password', {
+                                    required: 'El password es un dato obligatorio',
+                                    pattern: {
+                                        value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                                        message: 'El password debe contener entre 8 y 16 caracteres y debe incluir numeros, caracteres en mayuscula, miniscula y almenos un caracter especial'
+                                    }
+                                })
+                                }
                             />
                             <Form.Text className="text-danger">
-                                otro error
+                                {errors.password?.message}
                             </Form.Text>
                         </Form.Group>
                         <Button variant="primary" type="submit">
