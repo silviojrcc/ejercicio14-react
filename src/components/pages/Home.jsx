5fd5bbccaae8d9cc4683c1b89cc4847c2daeaa48
Silvio Ruiz <silviojrck@gmail.com>
2023-06-24 01:02:15 -0300

    Cambia la descripcion del sitio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         src={portada}
         alt="fondo cafe"
       />
-      <h3 className='text-center p-5 mx-5 d-md-none'>¡Bienvenido a PPRCommpling! La fusión perfecta entre programación y recetas de cocina para despertar tu creatividad en la cocina digital. ¡Explora, aprende y crea platos deliciosos mientras expandes tus habilidades de programación!</h3>
+      <h3 className='text-center p-5 mx-5 d-none d-md-block'>¡Bienvenido a PPRCommpling! La fusión perfecta entre programación y recetas de cocina para despertar tu creatividad en la cocina digital. ¡Explora, aprende y crea platos deliciosos mientras expandes tus habilidades de programación!</h3>
       <RecetasList />
     </section>
   );
